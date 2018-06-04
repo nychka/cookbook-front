2ff0acf7bcbcf344e965d2f29e4fa044e568e6e3
nychka <yaroslav_nychka@epam.com>
2018-06-04 16:39:16 +0300

    add bootstrap with material design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
 import React, {Component, Fragment} from 'react';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 import './App.css';
-import { Button} from 'react-bootstrap';
 import {  AsyncTypeahead } from 'react-bootstrap-typeahead';
+import { Button, Card, CardBody, CardImage, CardTitle, CardText } from 'mdbreact';
+import axios from 'axios';
+
+class Recipe extends Component{
+    render(){
+        return(
+            <Card style={{maxWidth: '22rem'}}>
+                <CardImage  className="img-thumbnail" src={ this.props.image } />
+                <CardBody>
+                    <CardTitle>{ this.props.title }</CardTitle>
+                    <CardText>{ this.props.description }</CardText>
+                    <Button href="#">Cook</Button>
+                </CardBody>
+            </Card>
+        )
+    }
+}
 
 class App extends Component {
     state = {
         isLoading: false,
-        options: [{id: 1, title: 'egg'}]
+        options: [/*{id: 1, title: 'egg'}*/],
+        api_host: 'http://localhost:3000',
+        recipes: []
     }
+
+    changeProductHandler = (products) => {
+        const titles = products.map(product => product.title);
+        const recipes = [];
+
+        console.log(titles);
+        if(titles.length == 0){
+            this.setState({ recipes: recipes });
+            return false;
+        }
+
+        console.log(titles);
+        axios.get(this.state.api_host + '/recipes?products=' + titles.join(','))
+             .then(response => {
+                console.log(response);
+                response.data.map(recipeData => {
+                    let recipe = <Recipe
+                        title={ recipeData.title }
+                        description={ recipeData.description }
+                        image={ recipeData.image }
+                    />
+                    recipes.push(recipe);
+                })
+
+                 this.setState({ recipes: recipes });
+             })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+
 
   render() {
     return (
@@ -30,9 +79,14 @@ class App extends Component {
                               options: json,
                           }));
                   }}
+                  onChange={this.changeProductHandler}
                   options={this.state.options}
               />
           </Fragment>
+
+          <div id="recipes_wrapper">
+              { this.state.recipes }
+          </div>
       </div>
     );
   }
